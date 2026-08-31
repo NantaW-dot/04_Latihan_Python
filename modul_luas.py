f83c28c9624933378a5811277e5ce5e26a676574
04_Arya Wirananta <arya.tkjsmeksa25@gmail.com>
2026-08-31 14:55:54 +0700

    Menambahkan Modul dan mengganti luas persegi panjang.py to modul_luas.py

---
+++ modul_luas.py
@@ -0,0 +1,21 @@
+def hitung_luas():
+    print("\n--- Program Hitung Luas ---")
+    print("Pilih bangun datar yang ingin dihitung:")
+    print("1. Persegi Panjang")
+    print("2. Persegi")
+
+    pilihan = input("Masukkan pilihan (1/2): ")
+
+    if pilihan == '1':
+        panjang = float(input("Masukkan panjang: "))
+        lebar = float(input("Masukkan lebar: "))
+        luas = panjang * lebar
+        print(f"Luas Persegi Panjang adalah: {luas}")
+        
+    elif pilihan == '2':
+        sisi = float(input("Masukkan panjang sisi: "))
+        luas = sisi * sisi
+        print(f"Luas Persegi adalah: {luas}")
+        
+    else:
+        print("Pilihan tidak valid. Silakan pilih 1 atau 2.")
