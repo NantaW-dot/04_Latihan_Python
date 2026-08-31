fda94034537be12052b22baf7f95ed436bd8d8b2
04_Arya Wirananta <arya.tkjsmeksa25@gmail.com>
2026-08-31 13:11:12 +0700

    Menambahkan hitungan Prima

--- Menghitung Bilangan Ganjil dan Genap.py
+++ Menghitung Bilangan Ganjil dan Genap.py
@@ -1,3 +1,12 @@
+def cek_prima(angka):
+    """Fungsi untuk menentukan apakah bilangan prima atau bukan."""
+    if angka <= 1:
+        return False
+    for i in range(2, int(angka**0.5) + 1):
+        if angka % i == 0:
+            return False
+    return True
+
 def cek_bilangan(angka):
     """Fungsi untuk menentukan apakah bilangan ganjil atau genap."""
     if angka % 2 == 0:
@@ -19,8 +28,13 @@ def main():
         if user_input.isdigit():
             angka = int(user_input)
             
-            hasil = cek_bilangan(angka)
-            print(hasil + "\n")
+            hasil_ganjil_genap = cek_bilangan(angka)
+            print(hasil_ganjil_genap)
+            
+            if cek_prima(angka):
+                print(f"{angka} Adalah Bilangan Prima.\n")
+            else:
+                print(f"{angka} Bukan Bilangan Prima.\n")
             
         else:
             print("Input tidak valid. Masukkan angka yang benar atau 'keluar'.\n")
