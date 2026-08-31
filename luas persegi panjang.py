1715b1687af7619f106a7ce65ea047797bcd423d
04_Arya Wirananta <arya.tkjsmeksa25@gmail.com>
2026-08-31 14:21:42 +0700

    Menambah Perhitungan Luas Pesegi

--- luas persegi panjang.py
+++ luas persegi panjang.py
@@ -1,6 +1,19 @@
-panjang = float(input("Masukkan Panjang persegi:  "))
-lebar = float(input("Masukkan Lebar Persegi:  "))
+print("Pilih bangun datar yang ingin dihitung:")
+print("1. Persegi Panjang")
+print("2. Persegi")
 
-luas = panjang * lebar
+pilihan = input("Masukkan pilihan (1/2): ")
 
-print(f"Luas persegi Panjang adalah: {luas}")
+if pilihan == '1':
+    panjang = float(input("Masukkan panjang: "))
+    lebar = float(input("Masukkan lebar: "))
+    luas = panjang * lebar
+    print(f"Luas Persegi Panjang adalah: {luas}")
+    
+elif pilihan == '2':
+    sisi = float(input("Masukkan panjang sisi: "))
+    luas = sisi * sisi
+    print(f"Luas Persegi adalah: {luas}")
+    
+else:
+    print("Pilihan tidak valid. Silakan jalankan ulang program dan pilih 1 atau 2.")
